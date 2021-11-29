50828bd77c2f2afa2b7db61391a864651446c6fe
Florian Coussy <florian.coussy@gmail.com>
2021-11-29 17:25:56 -0500

    Add FCC Tests bundle && Drum machine as a 3D object CSS draft

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,51 @@ import './App.scss';
 const App = () => {
   return (
     <div>
-      <h1>Drum Machine</h1>
+      <h1>Drum Maschine</h1>
+      <div id="drum-machine">
+        <div id="display">
+          <div id="key-Q" className="drum-pad">
+            Q
+            <audio id="Q" className="clip" src=""></audio>
+          </div>
+          <div id="key-W" className="drum-pad">
+            W
+            <audio id="Q" className="clip" src=""></audio>
+          </div>
+          <div id="key-E" className="drum-pad">
+            E
+            <audio id="Q" className="clip" src=""></audio>
+          </div>
+
+          <div id="key-A" className="drum-pad">
+            A
+            <audio id="Q" className="clip" src=""></audio>
+          </div>
+          <div id="key-S" className="drum-pad">
+            S
+            <audio id="Q" className="clip" src=""></audio>
+          </div>
+          <div id="key-D" className="drum-pad">
+            D
+            <audio id="Q" className="clip" src=""></audio>
+          </div>
+
+          <div id="key-Z" className="drum-pad">
+            Z
+            <audio id="Q" className="clip" src=""></audio>
+          </div>
+          <div id="key-X" className="drum-pad">
+            X
+            <audio id="Q" className="clip" src=""></audio>
+          </div>
+          <div id="key-C" className="drum-pad">
+            C
+            <audio id="Q" className="clip" src=""></audio>
+          </div>
+        </div>
+        <div id="top" />
+        <div id="left" />
+      </div>
     </div>
   );
 };
