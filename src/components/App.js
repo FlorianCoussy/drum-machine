c0c17c83b1f122ff0bc4944f8ec500b597bfc05a
Florian Coussy <florian.coussy@gmail.com>
2021-12-03 18:32:01 -0500

    Display 'drum machine' as a 3D box with animation

--- src/components/App.js
+++ src/components/App.js
@@ -5,49 +5,55 @@ const App = () => {
   return (
     <div>
       <h1>Drum Maschine</h1>
-      <div id="drum-machine">
-        <div id="display">
-          <div id="key-Q" className="drum-pad">
-            Q
-            <audio id="Q" className="clip" src=""></audio>
-          </div>
-          <div id="key-W" className="drum-pad">
-            W
-            <audio id="Q" className="clip" src=""></audio>
-          </div>
-          <div id="key-E" className="drum-pad">
-            E
-            <audio id="Q" className="clip" src=""></audio>
-          </div>
+      <div id="scene">
+        <div id="drum-machine">
+          <div id="display" className="box-face box-face-front">
+            <div id="key-Q" className="drum-pad">
+              Q
+              <audio id="Q" className="clip" src=""></audio>
+            </div>
+            <div id="key-W" className="drum-pad">
+              W
+              <audio id="Q" className="clip" src=""></audio>
+            </div>
+            <div id="key-E" className="drum-pad">
+              E
+              <audio id="Q" className="clip" src=""></audio>
+            </div>
 
-          <div id="key-A" className="drum-pad">
-            A
-            <audio id="Q" className="clip" src=""></audio>
-          </div>
-          <div id="key-S" className="drum-pad">
-            S
-            <audio id="Q" className="clip" src=""></audio>
-          </div>
-          <div id="key-D" className="drum-pad">
-            D
-            <audio id="Q" className="clip" src=""></audio>
-          </div>
+            <div id="key-A" className="drum-pad">
+              A
+              <audio id="Q" className="clip" src=""></audio>
+            </div>
+            <div id="key-S" className="drum-pad">
+              S
+              <audio id="Q" className="clip" src=""></audio>
+            </div>
+            <div id="key-D" className="drum-pad">
+              D
+              <audio id="Q" className="clip" src=""></audio>
+            </div>
 
-          <div id="key-Z" className="drum-pad">
-            Z
-            <audio id="Q" className="clip" src=""></audio>
+            <div id="key-Z" className="drum-pad">
+              Z
+              <audio id="Q" className="clip" src=""></audio>
+            </div>
+            <div id="key-X" className="drum-pad">
+              X
+              <audio id="Q" className="clip" src=""></audio>
+            </div>
+            <div id="key-C" className="drum-pad">
+              C
+              <audio id="Q" className="clip" src=""></audio>
+            </div>
           </div>
-          <div id="key-X" className="drum-pad">
-            X
-            <audio id="Q" className="clip" src=""></audio>
-          </div>
-          <div id="key-C" className="drum-pad">
-            C
-            <audio id="Q" className="clip" src=""></audio>
-          </div>
+          <div className="box-face box-face-back" />
+          <div className="box-face box-face-right" />
+          <div className="box-face box-face-left" />
+          <div className="box-face box-face-top" />
+          <div className="box-face box-face-bottom" />
         </div>
-        <div id="top" />
-        <div id="left" />
+        <div id="shadow" />
       </div>
     </div>
   );
