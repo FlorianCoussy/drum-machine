8c1bcddd0590b924cfbe32a58c0f39268ab2e069
Florian Coussy <florian.coussy@gmail.com>
2021-12-15 20:57:12 -0500

    Update 'VolumeSlider' style

--- src/components/atoms/VolumeSlider.js
+++ src/components/atoms/VolumeSlider.js
@@ -4,9 +4,16 @@ import './VolumeSlider.scss';
 const VolumeSlider = () => {
   return (
     <div id="volume-slider">
-      <div id="volume-lights"></div>
+      <div id="volume-lights">
+        {[...Array(15)].map((_, i) => (
+          <div className="light-dot active" key={i} />
+        ))}
+        {[...Array(10)].map((_, i) => (
+          <div className="light-dot" key={i} />
+        ))}
+      </div>
       <div id="slider">
-        <input type="range" />
+        <input type="range" min="0" max="100" step="5" />
       </div>
     </div>
   );
