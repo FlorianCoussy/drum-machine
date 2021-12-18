9f62e973eef407fa8bf7205197c3e7cec3d16172
Florian Coussy <florian.coussy@gmail.com>
2021-12-18 15:21:05 -0500

    Update 'IOButton' component

--- src/components/atoms/IOButton.js
+++ src/components/atoms/IOButton.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './IOButton.scss';
 
-const IOButton = () => {
-  const [isPlayOn, setIsPlayOn] = useState(false);
+const IOButton = ({ isTurnedOn, handleClick }) => (
+  <div id="io">
+    <button onClick={handleClick}>
+      <span className={isTurnedOn ? 'active' : ''}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-play-fill" viewBox="0 0 16 16">
+          <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
+        </svg>
+        Play
+      </span>
+    </button>
+  </div>
+);
 
-  const handleClick = () => setIsPlayOn(!isPlayOn);
-
-  return (
-    <div id="io">
-      <button onClick={handleClick}>
-        <span className={isPlayOn ? 'active' : ''}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-play-fill" viewBox="0 0 16 16">
-            <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
-          </svg>
-          Play
-        </span>
-      </button>
-    </div>
-  );
+IOButton.propTypes = {
+  isTurnedOn: PropTypes.bool.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default IOButton;
--- src/components/molecules/Controls.js
+++ src/components/molecules/Controls.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IOButton from '../atoms/IOButton';
 import PadNameDisplay from '../atoms/PadNameDisplay';
 import VolumeSlider from '../atoms/VolumeSlider';
 import './Controls.scss';
 
-const Controls = () => (
-  <div id="controls">
-    <div>
-      <div id="logo">&copy; MASCHINE</div>
-      <IOButton />
-    </div>
-    <PadNameDisplay />
-    <VolumeSlider />
-    <div className="banks">
-      <div className="bank">
-        <button>
-          <span>Bank 1</span>
-        </button>
+const Controls = () => {
+  const [isTurnedOn, setIsTurnedOn] = useState(false);
+
+  const handleIOButtonClick = () => setIsTurnedOn(!isTurnedOn);
+
+  return (
+    <div id="controls">
+      <div>
+        <div id="logo">&copy; MASCHINE</div>
+        <IOButton isTurnedOn={isTurnedOn} handleClick={handleIOButtonClick} />
       </div>
-      <div className="bank">
-        <button>
-          <span>Bank 2</span>
-        </button>
+      <PadNameDisplay />
+      <VolumeSlider />
+      <div className="banks">
+        <div className="bank">
+          <button>
+            <span>Bank 1</span>
+          </button>
+        </div>
+        <div className="bank">
+          <button>
+            <span>Bank 2</span>
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default Controls;
