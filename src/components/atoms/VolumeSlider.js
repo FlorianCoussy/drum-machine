708ac3d8b3ebb262cb3df596aea79bc8f8b62ff1
Florian Coussy <florian.coussy@gmail.com>
2021-12-27 19:13:51 -0500

    Update 'Pad' style

--- src/components/atoms/VolumeSlider.js
+++ src/components/atoms/VolumeSlider.js
@@ -1,22 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import './VolumeSlider.scss';
 
 const VolumeSlider = ({ isTurnedOn }) => {
   const [volume, setVolume] = useState(0);
 
+  const handleChange = ({ currentTarget }) => {
+    if (isTurnedOn) setVolume(currentTarget.value);
+  };
+
+  useEffect(() => isTurnedOn ? setVolume(15) : setVolume(0), [isTurnedOn]);
+
   return (
     <div id="volume-slider">
       <div id="volume-lights">
-        {[...Array(15)].map((_, i) => (
-          <div className="light-dot active" key={i} />
-        ))}
-        {[...Array(10)].map((_, i) => (
-          <div className="light-dot" key={i} />
+        {[...Array(25)].map((_, i) => (
+          <div
+            className={(isTurnedOn && volume > 0 && i <= volume - 1) ? 'light-dot active' : 'light-dot'}
+            key={i}
+          />
         ))}
       </div>
       <div id="slider">
-        <input type="range" min="0" max="100" step="5" value="0" />
+        <input
+          value={volume}
+          onChange={handleChange}
+          type="range"
+          min="0"
+          max="25"
+          step="1"
+        />
       </div>
     </div>
   );
--- src/components/molecules/Pad.js
+++ src/components/molecules/Pad.js
@@ -3,69 +3,63 @@ import './Pad.scss';
 
 const Pad = () => (
   <div id="pad">
-    <div className="container">
-      <div className="row">
-        <div id="key-Q" className="drum-pad col">
-          <button>
-            Q
-            <audio id="Q" className="clip" src=""></audio>
-          </button>
-        </div>
-        <div id="key-W" className="drum-pad col">
-          <button>
-            W
-            <audio id="Q" className="clip" src=""></audio>
-          </button>
-        </div>
-        <div id="key-E" className="drum-pad col">
-          <button>
-            E
-            <audio id="Q" className="clip" src=""></audio>
-          </button>
-        </div>
-      </div>
-
-      <div className="row">
-        <div id="key-A" className="drum-pad col">
-          <button>
-            A
-            <audio id="Q" className="clip" src=""></audio>
-          </button>
-        </div>
-        <div id="key-S" className="drum-pad col">
-          <button>
-            S
-            <audio id="Q" className="clip" src=""></audio>
-          </button>
-        </div>
-        <div id="key-D" className="drum-pad col">
-          <button>
-            D
-            <audio id="Q" className="clip" src=""></audio>
-          </button>
-        </div>
-      </div>
-      <div className="row">
-        <div id="key-Z" className="drum-pad col">
-          <button>
-            Z
-            <audio id="Q" className="clip" src=""></audio>
-          </button>
-        </div>
-        <div id="key-X" className="drum-pad col">
-          <button>
-            X
-            <audio id="Q" className="clip" src=""></audio>
-          </button>
-        </div>
-        <div id="key-C" className="drum-pad col">
-          <button>
-            C
-            <audio id="Q" className="clip" src=""></audio>
-          </button>
-        </div>
-      </div>
+    <div id="key-Q" className="drum-pad">
+      <button className="cyan">
+        <span>Q</span>
+        <audio id="Q" className="clip" src=""></audio>
+      </button>
     </div>
+    <div id="key-W" className="drum-pad">
+      <button className="cyan">
+        <span>W</span>
+        <audio id="Q" className="clip" src=""></audio>
+      </button>
+    </div>
+    <div id="key-E" className="drum-pad">
+      <button className="cyan">
+        <span>E</span>
+        <audio id="Q" className="clip" src=""></audio>
+      </button>
+    </div>
+
+    <div id="key-A" className="drum-pad">
+      <button className="purple">
+        <span>A</span>
+        <audio id="Q" className="clip" src=""></audio>
+      </button>
+    </div>
+    <div id="key-S" className="drum-pad">
+      <button className="purple">
+        <span>S</span>
+        <audio id="Q" className="clip" src=""></audio>
+      </button>
+    </div>
+    <div id="key-D" className="drum-pad">
+      <button className="purple">
+        <span>D</span>
+        <audio id="Q" className="clip" src=""></audio>
+      </button>
+    </div>
+
+    <div id="key-Z" className="drum-pad">
+      <button className="blue">
+        <span>Z</span>
+        <audio id="Q" className="clip" src=""></audio>
+      </button>
+    </div>
+    <div id="key-X" className="drum-pad">
+      <button className="blue">
+        <span>X</span>
+        <audio id="Q" className="clip" src=""></audio>
+      </button>
+    </div>
+    <div id="key-C" className="drum-pad">
+      <button className="blue">
+        <span>C</span>
+        <audio id="Q" className="clip" src=""></audio>
+      </button>
+    </div>
+
   </div>
 );
 
